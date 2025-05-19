7e8386c409b8c071880d9141983830868667d1e9
youic <youic999@gmail.com>
2025-05-19 12:04:23 +0900

    rout.tsの変更

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,25 +1,38 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-// async function listInvoices() {
+async function listInvoices() {
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+	return data;
+}
+
+// async function dropTables() {
 // 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
+//     DROP TABLE IF EXISTS users, customers, invoices, revenue CASCADE;;
 //   `;
 
 // 	return data;
 // }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
   // try {
-  // 	return Response.json(await listInvoices());
+  // 	return Response.json(await dropTables());
   // } catch (error) {
   // 	return Response.json({ error }, { status: 500 });
   // }
